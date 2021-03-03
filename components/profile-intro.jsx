4f5da80027648b676de9e654f27abcd3309ed905
Anindya Dey <Anindya.Dey@vistaprint.com>
2021-03-03 15:19:24 +0530

    add next/image to handle profile pic optimization

--- components/profile-intro.jsx
+++ components/profile-intro.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Image from 'next/image';
 
 export default function ProfileIntro({ profileIntro }) {
     return (
         <React.Fragment>
-          <img className='w-24 h-24 mx-auto rounded-full' src='profile-picture.jpg'/>
+          <div className='relative w-24 h-24 mx-auto'>
+            <Image 
+              className='rounded-full'
+              src='/images/profile.jpg'
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
           <h1 className='text-3xl font-light py-2 text-center'>{ profileIntro.name }</h1>
           <p className='text-md font-light text-center'>{ profileIntro.jobTitle }</p>
         </React.Fragment>
