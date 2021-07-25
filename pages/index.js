b829adfed4c77912e30f96e39a1955dd5006fe52
Anindya Dey <andy.ani1992@gmail.com>
2021-07-25 22:33:06 +0530

    preetify js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import 'tailwindcss/tailwind.css';
-import Head from 'next/head'
-import Image from 'next/image';
-import SocialLink from '../components/social-link';
-import { SocialLinks } from '../data/social-links';
+import "tailwindcss/tailwind.css";
+import Head from "next/head";
+import Image from "next/image";
+import SocialLink from "../components/social-link";
+import { SocialLinks } from "../data/social-links";
 
 export default function Home() {
   return (
@@ -11,33 +11,45 @@ export default function Home() {
         <title>Anindya Dey | Full-stack Developer</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div className='container mx-auto mt-4'>
-        <div className='flex px-6 py-3 flex justify-between items-center'>
+      <div className="container mx-auto mt-4">
+        <div className="flex px-6 py-3 flex justify-between items-center">
           <div>
-            <a href='#' className='text-4xl font-bold'>AD</a>
+            <a href="#" className="text-4xl font-bold">
+              AD
+            </a>
           </div>
-          <div className='space-x-6'>
-            <a href='#' className='text-lg font-extralight'>About Me</a>
-            <a href='#' className='px-6 py-2 border-black border-2 rounded-full text-lg font-extralight'>Say Hi!</a>
+          <div className="space-x-6">
+            <a href="#" className="text-lg font-extralight">
+              About Me
+            </a>
+            <a
+              href="#"
+              className="px-6 py-2 border-black border-2 rounded-full text-lg text-white bg-black hover:text-black hover:bg-white"
+            >
+              Say Hi!
+            </a>
           </div>
         </div>
-        <div className='mt-8 px-3 py-2 flex flex-col items-center'>
+        <div className="mt-8 px-3 py-2 flex flex-col items-center">
           <div>
-            <Image 
-              src='/images/profile.jpg'
+            <Image
+              src="/images/profile.jpg"
               height={200}
-              width={200} 
-              className='rounded-full' />
+              width={200}
+              className="rounded-full"
+            />
           </div>
-          <div className='mt-6 text-5xl font-light'>Full-stack Developer</div>
-          <div className='mt-2 text-lg text-gray-500'>In love with the world of 0s and 1s</div>
+          <div className="mt-6 text-5xl font-light">Full-stack Developer</div>
+          <div className="mt-2 text-lg text-gray-500">
+            In love with the world of 0s and 1s
+          </div>
         </div>
-        <div className='mt-8 px-3 py-2 flex justify-center space-x-4'>
-          {
-            SocialLinks.map((link, i) => <SocialLink key={i} url={link.url} icon={link.icon} />)
-          }
+        <div className="mt-8 px-3 py-2 flex justify-center space-x-4">
+          {SocialLinks.map((link, i) => (
+            <SocialLink key={i} url={link.url} icon={link.icon} />
+          ))}
         </div>
       </div>
     </div>
-  )
+  );
 }
