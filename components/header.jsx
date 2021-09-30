49396688c24983bf6de70ed77e688da802d22c33
Anindya Dey <30517208+andys-github@users.noreply.github.com>
2021-09-30 14:19:28 +0530

    v2 - update footer (#6)
    * update package
    
    * remove unused components for v2
    
    * update pkg and fix lint issues
    
    * remove duplicates from index.js
    
    * refactor components into header, footer and main-content
    
    * update pkg
    
    * update header and footer with inset values
    
    * update footer
    
    Co-authored-by: Anindya Dey <Anindya.Dey@vistaprint.com>

--- components/header.jsx
+++ components/header.jsx
@@ -1,24 +1,24 @@
 function Header() {
     return (
         <>
-            <div className="fixed top-0 z-10 w-full mb-20 px-6 py-3 shadow-lg flex justify-between items-center bg-gray-300">
+            <header className="z-10 fixed inset-x-0 top-0 h-20 opacity-90 px-6 shadow-lg flex justify-between items-center bg-gray-300">
                 <div>
                     <a href="#" className="text-4xl font-bold">
                         AD
                     </a>
                 </div>
                 <div className="space-x-6">
-                    <a href="#" className="text-lg font-extralight">
+                    <a href="#" className="text-xl sm:font-extralight">
                         About Me
                     </a>
                     <a
                         href="#"
-                        className="px-6 py-2 border-black border-2 rounded-full text-lg text-white bg-black hover:text-black hover:bg-white"
+                        className="px-6 py-2 border-gray-800 border-2 rounded-full text-xl text-gray-300 bg-gray-800 hover:text-gray-800 hover:bg-gray-300"
                     >
                         Say Hi!
                     </a>
                 </div>
-            </div>
+            </header>
         </>
     )
 }
