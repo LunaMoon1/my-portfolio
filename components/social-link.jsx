7e32468dcbfbe6ce42f99fb91e637b7eceaa57a9
Anindya Dey <30517208+andys-github@users.noreply.github.com>
2021-09-30 12:15:18 +0530

    v2 - refactor header, footer and content (#5)
    * update package
    
    * remove unused components for v2
    
    * update pkg and fix lint issues
    
    * remove duplicates from index.js
    
    * refactor components into header, footer and main-content
    
    * update pkg
    
    Co-authored-by: Anindya Dey <Anindya.Dey@vistaprint.com>

--- components/social-link.jsx
+++ components/social-link.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export default function SocialLink({ url, icon }) {
     return (
         <a href={url} className='text-gray-600' target='_blank' rel='noreferrer'>
-            <FontAwesomeIcon icon={icon} size='2x' />
+            <FontAwesomeIcon icon={icon} size='lg' />
         </a>
     );
 }
