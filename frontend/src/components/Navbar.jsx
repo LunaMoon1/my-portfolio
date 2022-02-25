58a641739effb6a4172fe6b946932e23987eac61
Anindya Dey <andy.ani1992@gmail.com>
2022-02-25 22:57:41 +0530

    add components

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div>
+            <img src={Image.url} />
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
