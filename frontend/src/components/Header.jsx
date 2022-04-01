5474e18777163422d311b886adba3e6918691f69
Anindya Dey <andy.ani1992@gmail.com>
2022-04-01 13:47:31 +0530

    update Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -16,32 +16,29 @@ const scaleVariants = {
 
 const Header = () => {
   return (
-    <div className="flex-1 flex-col w-full h-full items-center justify-center py-24 2xl:py-32  px-8 bg-cover bg-repeat" style={{ backgroundImage: `url(${images.bgIMG})` }}>
+    <div className="flex-1 flex-col w-full h-full items-center justify-center py-24 2xl:py-32 2xl:flex 2xl:flex-row px-8 bg-cover bg-repeat" style={{ backgroundImage: `url(${images.bgIMG})` }}>
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 0.75 }}
       >
-        <div className="w-full flex flex-col items-end">
-          <div className="flex items-center py-8 px-6 bg-white rounded-md shadow-md">
-            <span className="text-3xl">👋</span>
+        <div className="w-full flex flex-col items-start font-light text-gray-500">
+          <div className="flex items-center py-8 px-6 bg-white rounded-md shadow-lg">
+            <span className="text-4xl">👋</span>
             <div className="ml-5">
-              <p className="text-sm font-light text-gray-500">Hi, I am</p>
-              <h1 className="text-5xl font-light text-gray-600">Anindya</h1>
+              <p className="text-sm">Hi, I am</p>
+              <h1 className="text-3xl">Anindya Dey</h1>
             </div>
           </div>
-          <div className="flex flex-col items-end py-4 px-6 bg-white rounded-md shadow-md mt-8 uppercase text-sm">
+          <div className="flex flex-col items-start py-4 px-6 bg-white rounded-md shadow-lg mt-8 uppercase text-sm">
             <p className="py-2">
-              Software Developer
-            </p>
-            <p className="py-2">
-              Aspiring Blockchain Developer
+              Full-stack Developer
             </p>
           </div>
         </div>
       </motion.div>
 
       
-      {/* <motion.div
+      <motion.div
         whileInView={{ opacity: [0, 1] }}
         transition={{ duration: 0.5, delayChildren: 0.5 }}
       >
@@ -61,12 +58,12 @@ const Header = () => {
       >
         {
           [images.flutter, images.redux, images.sass].map((circle, index) => 
-            <div key={`circle-${index}`}>
+            <div key={`circle-${index}`} className="h-12 w-12 my-4 p-2 rounded-full bg-white shadow-lg">
               <img src={circle} alt="tech" />
             </div>
           )
         }
-      </motion.div> */}
+      </motion.div>
     </div>
   );
 };
