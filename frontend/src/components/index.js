761abf8ad8392174f68019430fe7eb9770d9b7a9
Anindya Dey <andy.ani1992@gmail.com>
2022-05-20 21:29:26 +0530

    clean portfolio

--- frontend/src/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Navbar } from "./Navbar";
-export { default as Header } from "./Header";
-export { default as About } from "./About";
-export { default as Work } from "./Work";
-export { default as Skills } from "./Skills";
-export { default as Testimonial } from "./Testimonial";
-export { default as Footer } from "./Footer";
